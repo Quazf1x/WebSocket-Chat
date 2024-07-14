262877defb412da9a7d1f0a5aec2441845b3c410
Quazf1x <103037316+Quazf1x@users.noreply.github.com>
2024-07-14 14:59:16 +0600

    Ver 0.0.2 bg configured

--- Client/src/components/App.tsx
+++ Client/src/components/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <main className="font-display">hi</main>;
+  return (
+    <main className="h-svh bg-300% animate-gradbg bg-gradient-to-tr from-violet-900 via-rose-400 to-orange-300 flex justify-center items-center">
+      <div className="bg-gray-100 opacity-80 p-4 h-5/6 w-4/6 rounded-3xl"></div>
+    </main>
+  );
 };
 
 export default App;
--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -2,6 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    backgroundSize: {
+      "300%": "300%",
+    },
+    animation: {
+      gradbg: "gradBg 15s ease-in-out infinite alternate",
+    },
+    keyframes: {
+      gradBg: {
+        "0%": {
+          backgroundPosition: "left",
+        },
+        "100%": {
+          backgroundPosition: "right",
+        },
+      },
+    },
     extend: {},
   },
   plugins: [],
